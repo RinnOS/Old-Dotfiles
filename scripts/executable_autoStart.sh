9a6f92fba25e9c024b63b1c75a19cf440b161494
Rinnray <rinnray@gliroid.com>
2022-07-16 05:45:00 +0200

    Update dot_config/i3/config Update dot_zshrc Update scripts/executable_autoStart.sh

--- scripts/executable_autoStart.sh
+++ scripts/executable_autoStart.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-startProgram() {
-    sleep $1
-    $2 &
+run(){
+    if ! pgrep $1 ;
+    then
+        $@&
+    fi
 }
 
-sh ./mountDrives.sh
+sh $HOME/scripts/monitorLayout.sh
+sleep 2
+sh $HOME/scripts/nvidiaPipeline.sh
 
-startProgram 3 "discord"
-startProgram 4 "obsidian"
-startProgram 3 "lutris"
-startProgram 3 "steam"
-startProgram 3 "pulsemeeter"
-startProgram 3 "/usr/bin/emacs --daemon"
+nitrogen --restore &
+
+sleep 5
+run "/usr/bin/emacs --daemon"
+#run discord
+
+#startProgram 3 "discord"
+#startProgram 4 "obsidian"
+#startProgram 3 "lutris"
+#startProgram 3 "steam"
+#startProgram 3 "pulsemeeter"
+#startProgram 3 "/usr/bin/emacs --daemon"
