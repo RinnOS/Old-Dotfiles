3aa75a34b912d080d070fd0b8f1cf4013241c3e5
Rinnray <rinnray@gliroid.com>
2022-07-18 21:17:59 +0200

    Update dot_config/i3/config Update dot_config/polybar/config.ini Update dot_config/polybar/executable_launch.sh

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -7,8 +7,9 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/middleBar.log /tmp/rightBar.log
+echo "---" | tee -a /tmp/middleBar.log /tmp/rightBar.log /tmp/leftBar.log
 polybar middle 2>&1 | tee -a /tmp/middleBar.log & disown
 polybar right 2>&1 | tee -a /tmp/rightBar.log & disown
+polybar left 2>&1 | tee -a /tmp/leftBar.log & disown
 
 echo "Bars launched..."
