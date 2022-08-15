b2de11ccfe6ff3f01d86152d217df416a105ef63
Rinnray <rinnray@gliroid.com>
2022-08-15 21:34:27 +0200

    Update dot_config/nvim/lua/user/autopairs.lua

--- dot_config/nvim/lua/user/autopairs.lua
+++ dot_config/nvim/lua/user/autopairs.lua
@@ -1,7 +1,5 @@
-local error = require('user/msg').error
 local status_ok, npairs = pcall(require, 'nvim-autopairs')
 if not status_ok then
-  error('Nvim-Autoparis', 'Nvim-Autoparis ran into some issues')
   return
 end
 
@@ -29,7 +27,6 @@ npairs.setup {
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp_status_ok, cmp = pcall(require, 'cmp')
 if not cmp_status_ok then
-  error('CMP', 'CMP ran into an issue in nvim-autopairs')
   return
 end
 cmp.event:on(
