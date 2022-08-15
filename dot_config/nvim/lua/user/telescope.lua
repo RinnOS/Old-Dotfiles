17d63676e88fd5d07d9a069d3594915d4a5a654e
Rinnray <rinnray@gliroid.com>
2022-08-15 21:36:11 +0200

    Update dot_config/nvim/lua/user/telescope.lua

--- dot_config/nvim/lua/user/telescope.lua
+++ dot_config/nvim/lua/user/telescope.lua
@@ -1,7 +1,5 @@
-local error = require('user/msg').error
 local status_ok, ts = pcall(require, 'telescope')
 if not status_ok then
-  error('Telescope', 'Telescope ran into an issue')
   return
 end
 
